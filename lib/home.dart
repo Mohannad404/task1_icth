338dbd47bbd97c9219854f8213a6e916d00acc28
unknown <mohannadhelp@gmail.com>
2023-09-12 20:26:58 +0300

    first comment

---
+++ lib/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'buttons.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Center(
+              child: Text(
+                'Welcome Back',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Image.asset('assets/img.png'),
+            const Button(txt: 'Get Started'),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Button(txt: 'Signup'),
+                SizedBox(
+                  width: 30,
+                ),
+                Button(txt: 'Login')
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
